cc70f5c8e0b495c8cfdb45ef5398ab54ffb56ddf
Aditya Swami <adityaswami2004@gmail.com>
2022-01-17 17:12:44 +0530

    DARKMODEGLITCH FIXED homepage complete

--- js/script.js
+++ js/script.js
@@ -3,10 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // DARK-MODE
 
+    var x=1;
     const checkbox= document.getElementById('dark-mode-checkbox');
+    const elementSideBar = document.getElementById('sidebar');
+    const elementHeader= document.getElementById('header');
+    const elementFooter= document.getElementById('footer');
+    const elementContent = document.getElementById('content');
+    const elementSideBarClose = document.getElementById('sidebar-button-close-img');
+    const elementSideBarOpen = document.getElementById('sidebar-button-open-img');
 
     checkbox.addEventListener('change', () => {
-      document.body.classList.toggle('dark-mode');
+      document.body.classList.toggle('darkmode');
+      elementSideBar.classList.toggle('darkmode');
+      elementHeader.classList.toggle('dark-mode');
+      elementFooter.classList.toggle('dark-mode');
+      elementContent.classList.toggle('dark-mode');
+      x++;
+      if (x%2==0) {
+        elementSideBarClose.src="/images/buttonw.png";
+        elementSideBarOpen.src="/images/button2w.png";
+      }
+      else {
+        elementSideBarClose.src="/images/button.png";
+        elementSideBarOpen.src="/images/button2.png";        
+      }
     });
 
 
@@ -22,8 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
     sideBarClose.addEventListener('click', () => {
       sideBar.classList.toggle('collapse');
     });
-    
-
 
     
 // CAROUSEL
@@ -33,18 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function showSlides() {
       var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
+      var slides = document.getElementsByClassName('mySlides');
+      var dots = document.getElementsByClassName('dot');
       for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = 'none';  
       }
       slideIndex++;
       if (slideIndex > slides.length) {slideIndex = 1}    
       for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" activecar", "");
+        dots[i].className = dots[i].className.replace(' activecar', '');
       }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " activecar";
+      slides[slideIndex-1].style.display = 'block';  
+      dots[slideIndex-1].className += ' activecar';
       setTimeout(showSlides, 4000);
     }
 
